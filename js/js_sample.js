5e76222e2d288c1d730332ae5129bbcbecc41bab
dcem <danhchoem.kg@gmail.com>
2019-04-24 01:00:39 +0700

    Remove slash

--- js/js_sample.js
+++ js/js_sample.js
@@ -2,7 +2,7 @@
 
     jQuery.fn.helloWorld = function(content,link1,link2) {
 //var link1='24h.com.vn';
-        //var link2='http://google.com';
+
 $('#dialog-form').remove();
         $('#cover').remove();
 $('body').append('<div id="dialog-form"><a href="#" class="cancel"><span class="top-right"></span>&times;</a><h3>Hellow world</h3><center><button onclick="location.href='+"'"+link1+"'"+';" id="yes">Ok</button><button onclick="location.href='+"'"+link2+"'"+';" id="no">Cancel</button></center>');
